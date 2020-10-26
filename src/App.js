2c79e8ce3f0aab3501b253f095df88ed884f1fb9
sdh7700 <donghwa811@naver.com>
2020-10-26 22:27:25 +0900

    # create components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
+import TodoCreate from './components/TodoCreate';
 import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 const GlobalStyle = createGlobalStyle`
@@ -15,6 +17,8 @@ function App() {
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead />
+        <TodoList />
+        <TodoCreate />
       </TodoTemplate>
     </>
   );
